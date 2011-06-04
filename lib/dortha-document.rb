04882c43068915ba9a8605886a474153e5f891b9
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-06-03 22:20:04 -0400

    Refactoring to DRY up my parser.

---
+++ lib/dortha-document.rb
@@ -0,0 +1,74 @@
+class Document < Array
+	def initialize(sourceFile)
+		super # Call ruby's built-in Array initializer.
+		@tokenStack = TokenStack.new
+		@lineCount = 0
+	end
+	def tokenStack
+		@tokenStack
+	end
+	def parse
+		self.each_with_index do |line,lineNumber|
+			line.lstrip! # Strip leading whitespace on each line.
+			until line.empty?
+				if line.match(/^\"/) # If line starts with a quote...
+					stripQuotedToken(lineNumber)
+				elsif line.match(/^\[/) # If line starts with a bracket...
+					stripBraketedToken(lineNumber)
+				elsif line.match(/\s/) # If line contains whitespace...
+					stripPlainToken(lineNumber)
+				else # If nothing else, it's probobly a line with only one token. In that case, just take it off the line.
+					stripSingleToken(lineNumber)
+				end
+			end
+		end
+	end
+	def stripSingleToken(lineNumber)
+		tokenLength = self[lineNumber].length # Find end of Token.
+		tokenString = self[lineNumber].slice!(0,tokenLength) # Remove token from line.
+		token = Token.new(@tokenStack) # Create a new Token.
+		token.lineNumber = lineNumber # Set it's line number.
+		token.value = tokenString # Set it's value.
+		token.save # Persist the token to the TokenStack.
+	end
+	def stripPlainToken(lineNumber)
+		tokenLength = self[lineNumber].index(/\s/) # Find end of Token (indicated by whitespace).
+		if tokenLength.nil?
+			raise "Token is not followed by whitespace! Please use stripSingleToken instead. Line was this: #{self.inspect}"
+		end
+		tokenString = self[lineNumber].slice!(0,tokenLength) # Remove token from line.
+		while self[lineNumber].match(/^\s/) # If characters after token are whitespace...
+			self[lineNumber].slice!(0) # Delete them.
+		end
+		token = Token.new(@tokenStack) # Create a new Token.
+		token.lineNumber = lineNumber # Set it's line number.
+		token.value = tokenString # Set it's value.
+		token.save # Persist the token to the TokenStack.
+	end
+	def stripQuotedToken(lineNumber)
+		self[lineNumber].slice!(0) # Remove leading quote.
+		endingQuote = self[lineNumber].index(/\"/) # Find next quote.
+		tokenString = self[lineNumber].slice!(0,endingQuote + 1) # Remove quoted token from line.
+		while self[lineNumber].match(/^\s/) # If characters after quoted token are whitespace...
+			self[lineNumber].slice!(0) # Delete them.
+		end
+		tokenString = '"' + tokenString # Put a quote back on the front of the string for storage.
+		token = Token.new(@tokenStack) # Create a new Token.
+		token.lineNumber = lineNumber # Set it's line number.
+		token.value = tokenString # Set it's value.
+		token.save # Persist the token to the TokenStack.
+	end
+	def stripBraketedToken(lineNumber)
+		self[lineNumber].slice!(0) # Remove leading braket.
+		endingBraket = self[lineNumber].index(/\]/) # Find next braket.
+		tokenString = self[lineNumber].slice!(0,endingBraket + 1) # Remove braketed token from line.
+		while self[lineNumber].match(/^\s/) # If characters after braketed token are whitespace...
+			self[lineNumber].slice!(0) # Delete them.
+		end
+		tokenString = '[' + tokenString # Put a braket back on the front of the string for storage.
+		token = Token.new(@tokenStack) # Create a new Token.
+		token.lineNumber = lineNumber # Set it's line number.
+		token.value = tokenString # Set it's value.
+		token.save # Persist the token to the TokenStack.
+	end
+end
---
+++ test/test_document.rb
@@ -0,0 +1,51 @@
+require '../lib/dortha-document.rb'
+require 'test/unit'
+
+class DocumentTest < Test::Unit::TestCase
+	def setup # Provided document should be an array representing lines from the source file, with newlines removed.
+		testArray = ["  test1 nexttTest1","test2 \"nextTest2\"","test3 [nextTest3]"] # Notice leading spaces.
+		@document = Document.new(testArray)
+	end
+	def test_newDocument
+		assert(@document) # Make sure object creation succeeded.
+		assert(@document.kind_of?(Array)) # Document is supposed to be a subclass of Array.
+		assert_not_nil(@document.tokenStack) # Should have a tokenStack to work with.
+	end
+	def test_parse
+		@document.parse
+		testLineNumbers = @document.tokenStack.lineNumbers
+		testLineNumber = testLineNumbers[3] # The fourth token.
+		assert_equal(1,testLineNumber,"The fourth token should be on line 2.") # Line numbers start at zero.
+		testTokens = @document.tokenStack.tokens
+		testToken = testTokens[3] # The fourth token.
+		assert_equal("\"nextTest2\"",testToken,"The fourth token should be \"nextTest2\".") # Line numbers start at zero.
+	end
+	def test_stripSingleToken
+		testLine = ['Token']
+		@document = Document.new(testLine)
+		@document.stripSingleToken(0) # This method expects a line number. We'll give it zero.
+		assert_equal([""],@document,"Token not striped.")
+		assert_equal(["Token"],@document.tokenStack.tokens,"Token on stack does not match what we put in.")
+	end
+	def test_stripPlainToken
+		testLine = ['Token  AnotherToken'] # Notice there are two spaces in the middle of this line.
+		@document = Document.new(testLine)
+		@document.stripPlainToken(0) # This method expects a line number. We'll give it zero.
+		assert_equal(["AnotherToken"],@document,"Token not striped.")
+		assert_equal(["Token"],@document.tokenStack.tokens,"Token on stack does not match what we put in.")
+	end
+	def test_stripQuotedToken
+		testLine = ['"Quoted Token"  non-quoted-token'] # Notice there are two spaces in the middle of this line.
+		@document = Document.new(testLine)
+		@document.stripQuotedToken(0) # This method expects a line number. We'll give it zero.
+		assert_equal(["non-quoted-token"],@document,"Quoted token not striped.")
+		assert_equal(["\"Quoted Token\""],@document.tokenStack.tokens,"Token on stack does not match what we put in.")
+	end
+	def test_stripBraketedToken
+		testLine = ['[Braket Token]  non-braket-token'] # Notice there are two spaces in the middle of this line.
+		@document = Document.new(testLine)
+		@document.stripBraketedToken(0) # This method expects a line number. We'll give it zero.
+		assert_equal(["non-braket-token"],@document,"Braketed token not striped.")
+		assert_equal(["[Braket Token]"],@document.tokenStack.tokens,"Token on stack does not match what we put in.")
+	end
+end
