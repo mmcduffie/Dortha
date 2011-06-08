f96321700055c88b0cb9afa44c422e8f52b2f63d
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-06-08 12:39:41 -0400

    Added tests and methods to TokenStore

--- lib/dortha-token-store.rb
+++ lib/dortha-token-store.rb
@@ -1,5 +1,24 @@
 class TokenStore
-	def inspectbleh
-		puts "BLEH!"
+	def initialize
+		@tokenStore = []
+	end
+	def tokenStore
+		@tokenStore
+	end
+	def addToken(value,line)
+		if @tokenStore[line]
+			addTokenToExistingLine(value,line)
+		else
+			addTokenToNewLine(value,line)
+		end
+	end
+	def addTokenToNewLine(value,line)
+		lineArray = []
+		lineArray[0] = value
+		@tokenStore[line] = lineArray
+	end
+	def addTokenToExistingLine(value,line)
+		lineArray = @tokenStore[line]
+		lineArray.push(value)
 	end
 end
--- test/test_token_store.rb
+++ test/test_token_store.rb
@@ -1,8 +1,34 @@
-require '../lib/dortha-token-stack.rb'
+require '../lib/dortha-token-store.rb'
 require 'test/unit'
 
 class TokenStoreTest < Test::Unit::TestCase
-	def test_inspectbleh
-		TokenStore.inspectbleh
+	def setup
+		@store = TokenStore.new
+	end
+	def test_addToken
+		@store.addToken("test1",3)
+		@store.addToken("test2",3)
+		testArray = @store.tokenStore
+		testLine = testArray[3]
+		testToken = testLine[0]
+		assert_equal("test1",testToken,"Token not what we expected.")
+		testToken = testLine[1]
+		assert_equal("test2",testToken,"Token not what we expected.")
+	end
+	def test_addTokenToNewLine
+		@store.addTokenToNewLine("test1",3)
+		testArray = @store.tokenStore
+		testLine = testArray[3]
+		assert_not_nil(testLine,"Line array not created")
+		testToken = testLine[0]
+		assert_equal("test1",testToken,"Token not what we expected.")
+	end
+	def test_addTokenToExistingLine
+		@store.addTokenToNewLine("test1",3)
+		@store.addTokenToExistingLine("test2",3)
+		testArray = @store.tokenStore
+		testLine = testArray[3]
+		testToken = testLine[1] # Notice this is the second token on line 3.
+		assert_equal("test2",testToken,"Token not what we expected.")
 	end
 end
