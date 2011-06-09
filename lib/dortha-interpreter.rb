b0b8d9063b77689d293fb1fbecf28316a4e74cf1
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-06-09 13:00:58 -0400

    Started working on my interpreter.

--- lib/dortha-interpreter.rb
+++ lib/dortha-interpreter.rb
@@ -1,4 +1,13 @@
 class Interpreter
-	def interpret(tokenStore)
+	def interpret(tokenStore,lineCount)
+		@tokenStore = tokenStore
+		@lineCount = lineCount
+		lineCount.times do |line|
+			currentReceiver = @tokenStore.receiver(line)
+			currentMessages = @tokenStore.messages(line)
+		end
+	end
+	def call(message,receiver)
+		return receiver
 	end
 end
--- test/test_interpreter.rb
+++ test/test_interpreter.rb
@@ -6,11 +6,17 @@ class InterpreterTest < Test::Unit::TestCase
 	def setup # Provided document should be an array representing lines from the source file, with newlines removed.
 		testArray = ["test1 middleMethod nexttTest1","test2 \"nextTest2\"","test3 [nextTest3]"] # Notice multiple methods per line.
 		@document = Document.new(testArray)
+		@interpreter = Interpreter.new
 	end
 	def test_interpret
 		@document.parse
 		lineCount = @document.lineCount
-		@interpreter = Interpreter.new
-		@interpreter.interpret(@document.tokenStore)
+		@interpreter.interpret(@document.tokenStore,lineCount)
+	end
+	def test_call
+		message = "testMethod"
+		receiver = "testObject"
+		testResponce = @interpreter.call(message,receiver)
+		assert_equal("testObject",testResponce,"The call method should always return the object it called.")
 	end
 end
