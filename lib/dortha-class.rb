1b9ada730a7cfa52fc6c622ee644bc79c5ef98b8
unknown <McDuffie@.(none)>
2011-06-10 19:22:59 -0400

    Added class methods.

--- lib/dortha-class.rb
+++ lib/dortha-class.rb
@@ -3,5 +3,27 @@
 # and syntax, it will be called "class".
 
 class Klass
-
+	def initialize
+		@superClass = nil
+		@classMethods = []
+	end
+	def superClass
+		@superClass
+	end
+	def superClass=(superClass)
+		@superClass = superClass
+	end
+	def addClassMethod(method)
+		@classMethods.push(method)
+	end
+	def classMethods
+		@classMethods
+	end
+	def checkForMethod(method)
+		if @classMethods.index(method)
+			return true
+		else
+			return false
+		end
+	end
 end
--- test/test_class.rb
+++ test/test_class.rb
@@ -3,9 +3,20 @@ require 'test/unit'
 
 class KlassTest < Test::Unit::TestCase
 	def setup
-	
+		@klass = Klass.new
 	end
-	def test_super
-		
+	def test_superClass
+		@klass.superClass = "foo"
+		test = @klass.superClass
+		assert_equal("foo",test,"Class should be what we set it to.")
+	end
+	def test_addClassMethod
+		@klass.addClassMethod("foo")
+		test = @klass.classMethods[0]
+		assert_equal("foo",test,"Class should have method we added to it.")
+	end
+	def test_checkForMethod
+		@klass.addClassMethod("foo")
+		assert(@klass.checkForMethod("foo"),"checkForMethod should return true.")
 	end
 end
