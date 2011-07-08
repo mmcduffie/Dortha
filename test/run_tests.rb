e2c2ba03b61a5365d3f9ddb488e254dae475fd39
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-07-08 13:53:09 -0400

    deleting stuff

--- test/run_tests.rb
+++
@@ -1,6 +0,0 @@
-Dir.glob('test*.rb').each do |file|
-	require file # Forces all tests to be part of test suite.
-end
-Dir.glob('../test/types/test*.rb').each do |file|
-	require file # Need a better way to load tests from subdirectories.
-end
