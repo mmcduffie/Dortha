d6c25fe047b4974426e81058b23fabe557937e05
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-07-12 13:51:50 -0400

    Added some docs.

--- lib/dortha.rb
+++ lib/dortha.rb
@@ -1,5 +1,12 @@
+# The Dortha module is the main namespace for all classes in Dortha.
+# To use Dortha, you can simply require this file (/lib/dortha.rb) and
+# you will have access to all mothods, modules, and classes in Dortha.
 module Dortha
   VERSION = '0.0.0'
+  # Returns the current version of Dortha.
+  def self.version
+    VERSION
+  end
   autoload :Document, "dortha/document"
   autoload :TokenStore, "dortha/token_store"
 end
--- lib/dortha/document.rb
+++ lib/dortha/document.rb
@@ -1,4 +1,6 @@
 module Dortha
+  # The Document class represents a Dortha source file, and contains
+  # methods that operate on each source file, including the lexer.
   class Document
     
   end
