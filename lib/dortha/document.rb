7bca46d37bba8569c5fbb2ab4374a258d4f231dc
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-07-14 13:00:50 -0400

    More work on Document class.

--- lib/dortha/document.rb
+++ lib/dortha/document.rb
@@ -6,32 +6,33 @@ module Dortha
     # super in our initializer for it to create an Array with our data.
     def initialize(source_file)
       super
-	  @line_count = 0
+      @line_count = 0
     end
     
     # The lex method is a lexer that takes all of the lines in the source file and 
     # converts each word into a Token object.
     def lex
-	  convert_lines_to_arrays
+      convert_lines_to_arrays
       self.each_with_index do |line,line_number|
-	    unless line_number == 0
+        unless line_number == 0
           line = line[0]
           line.lstrip!
           until line.empty?
-            #if line.match(/^\"/)
-              #strip_quoted_token(line_number)
-            if line.match(/\s/)
+            if line.match(/^\"/)
+              strip_quoted_token(line_number)
+            elsif line.match(/\s/)
               strip_plain_token(line_number)
             else
               strip_single_token(line_number)
             end
           end
-		end
-		@line_count += 1
+        end
+        @line_count += 1
       end
     end
     
     private
+    
       # line_count is incremented by the lexer as it converts lines to token objects.
       # it should reflect the total number of lines in the source file.
       attr_accessor :line_count
@@ -48,27 +49,38 @@ module Dortha
       # strip_single_token is used when there is only one word on a given line.
       # the word is removed from the line and replaced with a Token object.
       def strip_single_token(line_number)
-        line = self[line_number][0]
-        token_string = line.slice!(0,line.length)
+        line = self[line_number]
+        token_string = line[0].slice!(0,line[0].length)
         token = Dortha::Token.new(token_string,line_number)
-        self[line_number][0] = token
+        line[0] = token
       end
     
       # strip_plain_token is used when a line contains more than one token that is
       # not a double-quoted string. It will remove the token and replace it with a
       # Token object.
       def strip_plain_token(line_number)
-        line = self[line_number][0]
-        space_index = line.index(/\s/)
-        if space_index.nil?
-          raise "Token is not followed by whitespace! Please use stripSingleToken instead. Line was this: #{self.inspect}"
-        end
-        token_string = line.slice!(0,space_index)
-        while line.match(/^\s/)
-          line.slice!(0)
-        end
+        line = self[line_number]
+        space_index = line[0].index(/\s/)
+        token_string = line[0].slice!(0,space_index)
+        line[0].lstrip!
         token = Dortha::Token.new(token_string,line_number)
-        self[line_number].push(token)
+        line.push(token)
+      end
+      
+      # strip_quoted_token is used when the string we are converting to objects starts 
+      # with a double quote. This way, the lexer can make the entire quoted string into
+      # one token object, which is actualy going to be an object of the string class.
+      # this is the only data type that the lexer creates. The rest will be created by
+      # the interpreter.
+      def strip_quoted_token(line_number)
+        line = self[line_number]
+        line[0].slice!(0)
+        ending_quote = line[0].index(/\"/)
+        token_string = line[0].slice!(0,ending_quote + 1)
+        line[0].lstrip!
+        token_string.chomp!(34.chr)
+        token = Dortha::Token.new(token_string,line_number)
+        line.push(token)
       end
   end
 end
--- test/test_document.rb
+++ test/test_document.rb
@@ -2,36 +2,47 @@ require 'helper'
 
 class DocumentTest < Test::Unit::TestCase
   def setup
-    test_array = [nil,"  test1 nexttTest1","test2 \"nextTest2\"","test3 [nextTest3]"] # Notice leading spaces. Also notice first element is nil.
-	@document = Dortha::Document.new(test_array)
+    test_array = [nil,"  test1 nexttTest1","test2 \"nextTest2\"","test3 nextTest3"] # Notice leading spaces. Also notice first element is nil.
+    @document = Dortha::Document.new(test_array)
   end
   def test_lex
     before_test = @document
     @document.lex
+	puts @document.inspect
+	# TODO - Why aren't the tokens always in order?
   end
   def test_convert_lines_to_arrays
     def @document.fake_convert_lines_to_arrays
-	  convert_lines_to_arrays
-	end
+      convert_lines_to_arrays
+    end
     @document.fake_convert_lines_to_arrays
-	assert_equal [[nil],["  test1 nexttTest1"],["test2 \"nextTest2\""],["test3 [nextTest3]"]], @document
+    assert_equal [[nil],["  test1 nexttTest1"],["test2 \"nextTest2\""],["test3 nextTest3"]], @document
   end
   def test_strip_single_token
     test_line = [[nil],['Token']]
-	@document = Dortha::Document.new(test_line)
-	def @document.fake_strip_single_token(*args)
-	  strip_single_token(*args)
-	end
-	test = @document.fake_strip_single_token(1)
-	assert_equal "Token", @document[1][0].value, "Token string on line should be replaced with a Token object."
+    @document = Dortha::Document.new(test_line)
+    def @document.fake_strip_single_token(*args)
+      strip_single_token(*args)
+    end
+    test = @document.fake_strip_single_token(1)
+    assert_equal "Token", @document[1][0].value, "Token string on line should be replaced with a Token object."
   end
   def test_strip_plain_token
     test_line = [[nil],['Token  AnotherToken']] # Notice there are two spaces in the middle of this line.
-	@document = Dortha::Document.new(test_line)
-	def @document.fake_strip_plain_token(*args)
-	  strip_plain_token(*args)
-	end
-	test = @document.fake_strip_plain_token(1)
-	assert_equal "Token", @document[1][1].value, "Token string on line should be replaced with a Token object."
+    @document = Dortha::Document.new(test_line)
+    def @document.fake_strip_plain_token(*args)
+      strip_plain_token(*args)
+    end
+    test = @document.fake_strip_plain_token(1)
+    assert_equal "Token", @document[1][1].value, "Token string on line should be replaced with a Token object."
+  end
+  def test_strip_quoted_token
+    test_line = [[nil],['"Quoted Token"  non-quoted-token']]
+    @document = Dortha::Document.new(test_line)
+    def @document.fake_strip_quoted_token(*args)
+      strip_quoted_token(*args)
+    end
+    test = @document.fake_strip_quoted_token(1)
+    assert_equal "Quoted Token", @document[1][1].value, "Token object not what we expected."
   end
 end
