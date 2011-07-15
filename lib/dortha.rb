83ff5566266967cb96e696edba2efcbc669150d4
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-07-15 14:07:03 -0400

    Fixed main exec and binfile so they actually do somthing.

--- lib/dortha.rb
+++ lib/dortha.rb
@@ -2,12 +2,18 @@
 # To use Dortha, you can simply require this file (/lib/dortha.rb) and
 # you will have access to all mothods, modules, and classes in Dortha.
 module Dortha
+  
   VERSION = '0.0.0'
-  # Returns the current version of Dortha.
-  def self.version
-    VERSION
-  end
+  
   autoload :Document, "dortha/document"
   autoload :Token, "dortha/token"
   autoload :String, "dortha/types/string"
+
+  # This is the main exec for Dortha. This should get called weather we
+  # are invoking Dortha from the command line after insalling as a gem or 
+  # using Dortha as a required library.
+  def self.execute(source_file)
+    document = Dortha::Document.new(source_file)
+    document.lex
+  end
 end
--- test/test_document.rb
+++ test/test_document.rb
@@ -21,10 +21,10 @@ class DocumentTest < Test::Unit::TestCase
   
   def test_add_token
     @document = Dortha::Document.new([[nil],[]])
-    def @document.fake_add_token(*args)
+    def @document.proxy_add_token(*args)
       add_token(*args)
     end
-    @document.fake_add_token("test",1)
+    @document.proxy_add_token("test",1)
     test = @document[1][0]
     assert_equal "test", test.value, "Token value not correct."
   end
@@ -32,30 +32,30 @@ class DocumentTest < Test::Unit::TestCase
   def test_strip_single_token
     test_line = [[nil],['Token']]
     @document = Dortha::Document.new(test_line)
-    def @document.fake_strip_single_token(*args)
+    def @document.proxy_strip_single_token(*args)
       strip_single_token(*args)
     end
-    test = @document.fake_strip_single_token(1)
+    test = @document.proxy_strip_single_token(1)
     assert_equal "Token", @document[1][1].value, "Token string on line should be replaced with a Token object."
   end
   
   def test_strip_plain_token
     test_line = [[nil],['Token  AnotherToken']] # Notice there are two spaces in the middle of this line.
     @document = Dortha::Document.new(test_line)
-    def @document.fake_strip_plain_token(*args)
+    def @document.proxy_strip_plain_token(*args)
       strip_plain_token(*args)
     end
-    test = @document.fake_strip_plain_token(1)
+    test = @document.proxy_strip_plain_token(1)
     assert_equal "Token", @document[1][1].value, "Token string on line should be replaced with a Token object."
   end
   
   def test_strip_quoted_token
     test_line = [[nil],['"Quoted Token"  non-quoted-token']]
     @document = Dortha::Document.new(test_line)
-    def @document.fake_strip_quoted_token(*args)
+    def @document.proxy_strip_quoted_token(*args)
       strip_quoted_token(*args)
     end
-    test = @document.fake_strip_quoted_token(1)
+    test = @document.proxy_strip_quoted_token(1)
     assert_equal "Quoted Token", @document[1][1].value, "Token object not what we expected."
     assert_equal Dortha::String, @document[1][1].class, "Token should be of class Dortha::String."
   end
