ee5151f6588dfd9d8c715b92dfe4bd6ecb63ec7d
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-07-16 09:54:01 -0400

    Added detect_number_types method and test to Line.

--- lib/dortha/line.rb
+++ lib/dortha/line.rb
@@ -5,11 +5,13 @@ module Dortha
   # converted by methods in this class into objects of other types and then 
   # executed to do actual work.
   class Line < Array
+  
     def initialize(value)
       @value = value
       super
     end
     attr_accessor :value
+    
     # The 'contains_value?' method searches a line and returns true if a object
     # with the given value is found.
     def contains_value?(value_to_find)
@@ -18,6 +20,15 @@ module Dortha
         return false
       elsif results.length > 0
         return true
+      end
+    end
+    
+    # The detect_number_types method scans a Line of tokens for Tokens that can
+    # be turned into Numbers. If it finds them, in converts them to Number type
+    # objects.
+    def detect_number_types
+      self.map! do |token|
+        token = Dortha::Number.new(token.value,1) rescue token = token
       end
     end
   end
--- test/test_line.rb
+++ test/test_line.rb
@@ -12,7 +12,21 @@ class LineTest < Test::Unit::TestCase
     object2 = MockToken.new
     object2.value = "bar"
     line = Dortha::Line.new([object1,object2])
-    assert_equal true,line.contains_value?("foo")
-    assert_equal false,line.contains_value?("bleh")
+    assert_equal true, line.contains_value?("foo")
+    assert_equal false, line.contains_value?("bleh")
+  end
+  
+  def test_detect_number_types
+    object1 = MockToken.new
+    object1.value = "1"
+    object2 = MockToken.new
+    object2.value = "0"
+	object3 = MockToken.new
+    object3.value = "foo"
+    line = Dortha::Line.new([object1,object2,object3])
+    line.detect_number_types
+	assert_equal Dortha::Number, line[0].class, "First object should be of class 'Number'."
+	assert_equal Dortha::Number, line[1].class, "Second object should be of class 'Number', even though it was a zero."
+	assert_equal MockToken, line[2].class, "Last object should be unchanged."
   end
 end
