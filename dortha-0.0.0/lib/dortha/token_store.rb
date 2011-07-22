479d9536a4d7847e27956a74c98f23b37a09c411
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-07-22 15:22:47 -0400

    Removed old files

--- dortha-0.0.0/lib/dortha/token_store.rb
+++
@@ -1,8 +0,0 @@
-module Dortha
-  # The TokenStore class is a temporary storage space for token
-  # objects created by the lexer. These will be read by the interpreter
-  # when the program is executed.
-  class TokenStore
-    
-  end
-end
--- dortha-0.0.0/test/test_document.rb
+++
@@ -1,8 +0,0 @@
-require 'helper'
-
-class DocumentTest < Test::Unit::TestCase
-  def setup
-    testArray = [nil,"  test1 nexttTest1","test2 \"nextTest2\"","test3 [nextTest3]"] # Notice leading spaces. Also notice first element is nil.
-	@document = Dortha::Document.new(testArray)
-  end
-end
