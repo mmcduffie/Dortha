51726e5b2f126b84c594be4abba57f15d0cee1b0
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-07-22 16:14:16 -0400

    Added methods to handle whitespace cleanup.

--- lib/dortha/document.rb
+++ lib/dortha/document.rb
@@ -2,16 +2,29 @@ module Dortha
   # The Document class represents a Dortha source file, and contains
   # methods that operate on each source file, including the lexer.
   class Document < Array
-    # Creates a new Document. Document is a subclass of Array, so we have to call
-    # super in our initializer for it to create a Array with our data.
+    # Creates a new Document. To create a new Document you need to 
+    # provide the initalizer with a string.
     def initialize(source_file)
       code = source_file.gsub!(/(\n|\r)/,"").split(".")
       super(code)
     end
     
-    # The lex method is a lexer that takes all of the lines in the source file and 
+    # The lex method is a lexer that takes all of the sentences in the source file and 
     # converts each word into a Token object.
     def lex
+      remove_leading_whitespace
+      compact_remaining_whitespace
+    end
+    
+    # The remove_whitespace method deletes leading whitespace from sentences.
+    def remove_leading_whitespace
+      self.map! { |sentence| sentence = sentence.lstrip }
+    end
+    
+    # The compact_remaining_whitespace method finds whitespace more than one character
+    # long in the middle of sentences and replaces it with single spaces.
+    def compact_remaining_whitespace
+      self.map! { |sentence| sentence = sentence.gsub(/\s{2,}/," ") }
     end
   end
 end
--- test/test_document.rb
+++ test/test_document.rb
@@ -3,7 +3,7 @@ require 'helper'
 class DocumentTest < Test::Unit::TestCase
   
   def setup
-    test_array = "  test1 nexttTest1.\ntest2 \"nextTest2\".\ntest3 nextTest3."
+    test_array = "  test1  		nextTest1\n test2 \"nextTest2\".\n test3 nextTest3."
     @document = Dortha::Document.new(test_array)
   end
   
